b2954479dc6c58f025abc24dff9794703a467222
wpseemol <wpseemol@gmail.com>
2024-12-26 23:31:10 +0600

    created plugin main file and plugin related rewuirments

---
+++ Related-Posts.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * Plugin Name:       Related Posts
+ * Plugin URI:        #
+ * Description:       Show Related Post in single post page.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Seemol chakroborti
+ * Author URI:        https://github.com/wpseemol
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       related-posts
+ * Domain Path:       /languages
+ */
